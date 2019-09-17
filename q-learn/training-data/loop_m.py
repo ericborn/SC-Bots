11aa5291596d2139026780deefd2ec18528d7742
Eric Born <eric.born85@gmail.com>
2019-09-16 20:24:42 -0700

    Training data
    reworked folder structure. Added all training data.

--- q-learn/training-data/loop_m.py
+++
@@ -1,7 +0,0 @@
-import Binary_bot_m as bb
-
-i = 0
-while i < 2000:
-    print('running time', str(i))
-    bb.main()
-    i += 1
---
+++ q-learn/training/loop_e.py
@@ -0,0 +1,7 @@
+import Binary_bot_e as bb
+
+i = 0
+while i < 2000:
+    print('running time', str(i))
+    bb.main()
+    i += 1
